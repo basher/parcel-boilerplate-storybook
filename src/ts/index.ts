31aa9281a6d2be38544faf94b5d85ecf4ce88f14
basher <basher@madasafish.com>
2021-09-02 08:50:26 +0100

    add domready to mimic production repo

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,13 +1,21 @@
 import '../scss/index.scss';
 import { browserSupportsAllFeatures } from './test';
+import domready from 'domready';
 
 /* eslint-disable no-console */
 console.log(`Modern browser = ${browserSupportsAllFeatures()}`);
 
-if (!browserSupportsAllFeatures()) {
-    // Require 'promise' for Parcel, then dynamically import polyfills.
-    require('core-js/es/promise');
-    import('./polyfills')
-        .then(() => console.log('Polyfills loaded'))
-        .catch((e) => console.error(e));
-}
+// Run UI module instantiation when DOM is ready.
+domready(function () {
+    if (browserSupportsAllFeatures()) {
+        console.log('Instantiate UI modules...');
+    } else {
+        // Require 'promise' for Parcel, then dynamic import polyfills, then instantiate UI modules.
+        require('core-js/es/promise');
+        import('./polyfills')
+            .then(() =>
+                console.log('Polyfills loaded... instantiate UI modules...')
+            )
+            .catch((e) => console.error(e));
+    }
+});
